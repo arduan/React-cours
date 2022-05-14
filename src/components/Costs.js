1333675d11a2c37552ac6654a76a31219e6e5313
Vitaly <ivanov.arduan@yandex.ru>
2022-05-14 07:50:24 +0300

    Альтернативная запись функции - стрелочная функкция.

--- src/components/Costs.js
+++ src/components/Costs.js
@@ -1,13 +1,13 @@
 import Costitem from "./Costitem";
 import './Costs.css'
 
-function Costs(props) {
+const Costs=(props)=> {
     return (
         <div className='costs'>
 
-            <Costitem date={props.costs[0].date} description = {props.costs[0].description} amount ={props.costs[0].amount}></Costitem>
-            <Costitem date={props.costs[1].date} description = {props.costs[1].description} amount ={props.costs[1].amount}></Costitem>
-            <Costitem date={props.costs[2].date} description = {props.costs[2].description} amount ={props.costs[2].amount}></Costitem>
+            <Costitem date={props.costs[0].date} description = {props.costs[0].description} amount ={props.costs[0].amount}/>
+            <Costitem date={props.costs[1].date} description = {props.costs[1].description} amount ={props.costs[1].amount}/>
+            <Costitem date={props.costs[2].date} description = {props.costs[2].description} amount ={props.costs[2].amount}/>
 
         </div>
     );
