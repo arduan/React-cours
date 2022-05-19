0393b7d76e9aceb095c5c83448e5a509e50f65ee
Vitaly <ivanov.arduan@yandex.ru>
2022-05-19 09:35:26 +0300

    Сохреннение значения в состояние - states.

--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,25 +1,40 @@
-
+import React, {useState} from "react";
 import './NewCost.css';
 
 const CostForm = () => {
-    const nameChangeHendler = (event) => {
-        console.log(event.target.value);
+    const [name, setName] = useState('');
+    const [amount, setAmount] = useState('');
+    const [date, setDate] = useState('');
+
+
+
+    const nameChangeHandler = (event) => {
+       setName(event.target.value);
+    };
+    const amountChangeHandler = (event) => {
+        setAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+        setDate(event.target.value)
 
     };
+
+
     return <div>
         <form>
             <div className='new-cost__controls'>
                 <div className='new-cost__control'>
                     <label>Название</label>
-                    <input type="text" onChange={nameChangeHendler}/>
+                    <input type="text" onChange={nameChangeHandler}/>
                 </div>
                 <div className='new-cost__control'>
                     <label>Сумма</label>
-                    <input type="number" min='0.01' step='0.01'/>
+                    <input onChange={amountChangeHandler} type="number" min='0.01' step='0.01'/>
                 </div>
                 <div className='new-cost__control'>
                     <label>Дата</label>
-                    <input type="date" min='2019-01-01' step='2022-09-01' />
+                    <input onChange={dateChangeHandler} type="date" min='2019-01-01' step='2022-09-01' />
 
                 </div>
                 <div className='new-cost__actions'>
