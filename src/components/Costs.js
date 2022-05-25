0d039bb61392b5d67f0c847562324602cdfe6b97
Vitaly <ivanov.arduan@yandex.ru>
2022-05-25 07:23:30 +0300

    Продолжение.

--- src/components/Costs.js
+++ src/components/Costs.js
@@ -2,6 +2,7 @@ import Costitem from "./Costitem";
 import './Costs.css'
 import CostsFilter from "./Costs/CostsFilter";
 import React, {useState} from "react";
+import CostList from "./Costs/CostList";
 
 const Costs=(props)=> {
     const [selectedYear, setSelectedYear] = useState('2021');
@@ -12,23 +13,13 @@ const Costs=(props)=> {
         return cost.date.getFullYear().toString() === selectedYear;
     });
 
-    let costsContent = <p>В Этом Году Расходов Нет</p>;
-    if (filteredCosts.length > 0){
-       costsContent = filteredCosts.map((cost) =>(
-        <Costitem
-            key={cost.id}
-            date={cost.date}
-            description = {cost.description}
-            amount ={cost.amount}/>
 
-        ))
-    }
     return (
         <div>
 
         <div className='costs'>
             <CostsFilter year={selectedYear} onChangeYear={ yearChangeHandler}/>
-            {costsContent}
+            <CostList costs={ filteredCosts}/>
         </div>
         </div>
     );
--- src/components/Costs/CostList.js
+++ src/components/Costs/CostList.js
@@ -1,3 +1,22 @@
+import Costitem from "../Costitem";
+import React from "react";
+import './CostList.css'
+
+
 const CostList = (props) => {
-    
+    if (props.costs.length ===0){
+        return <h2 className='cost-list__fallback'>В Этом Году Расходов Нет</h2>
+
+    }
+    return <ul className='cost-list'>
+        { props.costs.map((cost) =>(
+        <Costitem
+            key={cost.id}
+            date={cost.date}
+            description = {cost.description}
+            amount ={cost.amount}/>
+
+        ))};
+    </ul>
 }
+export default CostList
